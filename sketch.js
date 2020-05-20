9a665211217de1df2a1d7867a6256511b8e4b9d7
recurn <61036489+recurn@users.noreply.github.com>
2020-05-20 11:43:16 -0700

    creation of day 2

--- sketch.js
+++ sketch.js
@@ -1,30 +1,22 @@
 let c;
-let day = 1;
+let day = 2;
 function setup() {
     c = createCanvas(400, 400);
     background(255);
     fill(0);
-    noStroke();
-    rectMode(CENTER);
-    let noiseAlpha = random(0,1);
-    let alphaStep = 0.1;
-    let noiseX = random(0, 1);
-    let noiseStep = 0.2;
-    let noiseY = random(0,1);
-    let noiseSize = random(0,1);
-    let sizeStep = 0.4;
-    for (let i = 20; i < width - 10; i += 10){
-        noiseX += noiseStep;
-        noiseSize += sizeStep;
-        for (g = 20; g < height - 10; g += 10){
-            noiseY += noiseStep * 2;
-            noiseAlpha += alphaStep;
-            noiseSize += sizeStep;
-            let alpha = noise(noiseAlpha);
-            let darkness = noise(noiseX, noiseY);
-            let size = noise(noiseSize) * 50;
-            fill(0, 150, darkness * 255, alpha * 200);
-            rect(i,g, size, size);
+    let xStep = 20;
+    let yStep = 50;
+    let variation = 0;
+    let noise = 0;
+    for (let y = 20; y < height - yStep; y += yStep){
+        let startY = y;
+        variation = 0;
+        for (x = 20; x < width - xStep; x += xStep){
+            let randomAmplitude = y + random(-variation, variation) / 2;
+            line(x,startY, x + xStep, randomAmplitude);
+            startY = randomAmplitude;
+            variation += noise;
+            noise += 0.04;
         }
     }
   }

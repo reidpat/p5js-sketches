3b349a10b6778ed9b8d13e9d5d4657783c19953c
recurn <61036489+recurn@users.noreply.github.com>
2020-05-21 08:13:57 -0700

    inkblot complete

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
 let c;
-let day = 0;
+let day = 3;
 function setup() {
-    c = createCanvas(400, 400);
-    background(255);
-  }
+      c = createCanvas(400, 400);
+      background(250);
+      let radius = 150;
+      translate(width / 2, height / 2);
+      let noiseSeed = random(1);
+      let noiseStep = 0.03;
+      fill(0);
+      beginShape();
+      let radNoise;
+      let startValue = noise(noiseSeed) * radius;
+      for (let d = 0; d < 360; d += 1) {
+            let rads = radians(d);
+            if (d > 340) {
+                  let diff = d - 360;
+                  radNoise = startValue + noise(noiseSeed) * diff;
+            }
+            else {
+                 
+                  radNoise = noise(noiseSeed) * radius;                 
+            }
+            let x = sin(rads) * radNoise;
+            let y = cos(rads) * radNoise;
+            noiseSeed += noiseStep;
+            vertex(x, y);
+      }
+      endShape();
+}
 
-  function mousePressed(){
+function keyPressed() {
+      if (key == 's'){
+      save(c, "day: " + day);
+      }
+}
 
-        save(c, "day: " + day);
-  }
-  
-  function draw(){
-  }
+function draw() {
+}

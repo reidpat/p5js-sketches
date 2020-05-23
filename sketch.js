55d65fc24c3e4e917dc38b7828a059b21013766a
recurn <61036489+recurn@users.noreply.github.com>
2020-05-23 10:21:36 -0700

    noise angle circle

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
 let c;
-let day = 0;
+let day = 5;
+
+
+let noiseDeg;
+let degNoiseStep = 0.1;
+let noiseLength;
+let lengthNoiseStep = 0.1
+
 function setup() {
-    c = createCanvas(400, 400);
-    background(255);
-  }
+      c = createCanvas(400, 400);
+      background(255);
+      angleMode(DEGREES);
+      translate(width/2, height/2);
+      noiseDeg = random(0,1);
+      noiseLength = random(0,1);
+      ellipse(0, 0, 10, 10);
+      strokeWeight(10);
+      stroke(0, 40);
+}
 
-  function mousePressed(){
+function keyPressed() {
+      if (key == 's') {
+            save(c, "day: " + day);
+      }
+}
 
-        save(c, "day: " + day);
-  }
-  
-  function draw(){
-  }
+function draw() {
+      translate(width/2, height/2);
+      let angle = noise(noiseDeg) * 360;
+      let length = noise(noiseLength) * 200;
+      let x = sin(angle) * length;
+      let y = cos(angle) * length;
+      line(-x,-y, x, y);
+     
+
+      noiseDeg += degNoiseStep;
+      noiseLength += lengthNoiseStep;
+}

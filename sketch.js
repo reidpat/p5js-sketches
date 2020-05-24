c37c33dde274001b858dafc69853ec736aa622f6
recurn <61036489+recurn@users.noreply.github.com>
2020-05-24 13:49:40 -0700

    petal explosion

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 let c;
-let day = 5;
+let day = 6;
 
 
 let noiseDeg;
 let degNoiseStep = 0.1;
 let noiseLength;
 let lengthNoiseStep = 0.1
+let rotation = 0;
 
 function setup() {
-      c = createCanvas(400, 400);
-      background(255);
+      c = createCanvas(500, 500);
+      background(250);
       angleMode(DEGREES);
-      translate(width/2, height/2);
-      noiseDeg = random(0,1);
-      noiseLength = random(0,1);
-      ellipse(0, 0, 10, 10);
-      strokeWeight(10);
-      stroke(0, 40);
+      translate(width / 2, height / 2);
+      noiseDeg = random(0, 1);
+      noiseLength = random(0, 1);
+      strokeWeight(1);
+      stroke(0,0);
+      noLoop();
+
+
 }
 
 function keyPressed() {
@@ -26,14 +29,23 @@ function keyPressed() {
 }
 
 function draw() {
-      translate(width/2, height/2);
-      let angle = noise(noiseDeg) * 360;
-      let length = noise(noiseLength) * 200;
-      let x = sin(angle) * length;
-      let y = cos(angle) * length;
-      line(-x,-y, x, y);
-     
+      for (let i = 0; i < 1000; i++) {
+            push();
+            translate(width / 2, height / 2);
+            rotate(rotation);
+            fill(0, random(0,255));
+            let angle = noise(noiseDeg) * 360;
+            let length = noise(noiseLength) * 350;
+            let x1 = sin(angle) * length;
+            let y1 = cos(angle) * length;
+            let x2 = sin(angle - random(0, 1) * 360) * length;
+            let y2 = sin(angle - random(0, 1 * 360)) * length;
+            curve(x1, y1, x2, y2, x2, y2, -x1, -y1);
 
-      noiseDeg += degNoiseStep;
-      noiseLength += lengthNoiseStep;
+            noiseDeg += degNoiseStep;
+            noiseLength += lengthNoiseStep;
+            rotation += 5;
+            pop();
+      }
+
 }

ea6fa1ea3de37612cf94dfb481da840be2cc3b96
recurn <61036489+recurn@users.noreply.github.com>
2020-05-26 09:05:35 -0700

    sin waves

--- sketch.js
+++ sketch.js
@@ -6,6 +6,10 @@ let day = 7;
 let capturer; 
 let btn;
 
+
+let ballList = [];
+
+
 function record() {
       capturer = new CCapture({
             format: "webm",
@@ -27,22 +31,46 @@ function setup() {
       c = p5canvas.canvas;
       background(250);
       fill(255, 0);
+      angleMode(DEGREES);
       frameRate(24)
       btn = document.createElement("button");
       btn.textContent = "start recording";
       document.body.appendChild(btn);
       btn.onclick = record;
       //btn.click(); //start recording automatically
+      let gap = 20;
+      for (let y = -150; y < height + gap; y += gap){
+            for (x = -150; x < width + 100; x += 5){
+                  ballList.push({
+                        x: x,
+                        y: y
+                  })
+            }
+
+      }
 }
 
 function keyPressed() {
       if (key == 's') {
-            capturer.save();
-            //save(c, "day: " + day);
+            //capturer.save();
+            save(c, "day: " + day);
       }
 }
 
 function draw() {
+      translate(width/2, 0);
+      rotate(45);
+      background(250);
+      ballList.forEach(ball => {
+            let x = ball.x;
+            let y = ball.y;
+            //anglemode(RADIANS);
+            ellipse(x, y + sin(x * 3) * 10, 2, 2);
+            ball.x += 1;
+            if(ball.x >= width + 100){
+                  ball.x = -150;
+            }
+      })
       if (capturer) {
             capturer.capture(document.getElementById("defaultCanvas0"));
       }

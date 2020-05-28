626d74ba307eec4f7e5ddec3d0c693d03208bf52
recurn <61036489+recurn@users.noreply.github.com>
2020-05-28 14:07:03 -0700

    spooky tree

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let c;
-let day = 7;
+let day = 10;
 
 
 
-let capturer; 
+let capturer;
 let btn;
 
 function record() {
@@ -25,7 +25,11 @@ function record() {
 function setup() {
       let p5canvas = createCanvas(400, 400);
       c = p5canvas.canvas;
-      background(250);
+      background(50);
+      background(0, 0, 100, 100);
+      noStroke();
+      ellipse(width/2,height/2,150,150);
+      stroke(0);
       fill(255, 0);
       frameRate(24)
       btn = document.createElement("button");
@@ -33,12 +37,34 @@ function setup() {
       document.body.appendChild(btn);
       btn.onclick = record;
       //btn.click(); //start recording automatically
+      createLine(width / 2, height, 200);
+}
+
+function createLine(x, y, length) {
+      if (length < 20) {
+            line(x, y, x + 5, y - 5)
+      }
+      else {
+
+            singleLine(x,y,length, -1);
+            singleLine(x,y,length, 1)
+      }
+
+}
+
+function singleLine(x, y, length, dir) {
+      let newX = x + (random(5, length) * dir) / 1.4;
+      let newY = y - random(5, length);
+      let newLength = length/1.5;
+      strokeWeight(length/10);
+      line(x, y, newX, newY);
+      createLine(newX, newY, newLength);
 }
 
 function keyPressed() {
       if (key == 's') {
-            capturer.save();
-            //save(c, "day: " + day);
+            //capturer.save();
+            saveCanvas(c, "day: " + day, 'png');
       }
 }
 
@@ -46,4 +72,5 @@ function draw() {
       if (capturer) {
             capturer.capture(document.getElementById("defaultCanvas0"));
       }
+
 }
